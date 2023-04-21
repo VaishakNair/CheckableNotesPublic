136dfd1dbb7725048fc3d3c3d0dda0228f197787
VaishakNair <vaishak.k.nair@gmail.com>
2023-04-21 21:06:44 +0530

    Wire list updation logic using set subtraction.

--- app/src/main/java/in/v89bhp/checkablenotes/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/in/v89bhp/checkablenotes/viewmodels/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package `in`.v89bhp.checkablenotes.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,18 +9,61 @@ import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.lifecycle.ViewModel
 import `in`.v89bhp.checkablenotes.composables.CheckableItem
+import kotlinx.coroutines.MainScope
 
 class MainActivityViewModel : ViewModel() {
+
+    val TAG = "MainActivityViewModel"
 
     var text by mutableStateOf(TextFieldValue("", TextRange(0, 7)))
 
     val list = mutableListOf<CheckableItem>().toMutableStateList()
 
+
     fun updateList(value: TextFieldValue) {
-        list.removeAll { true }
-        list.addAll(value.text.split('\n').mapIndexed { index, value ->
+        val newList = value.text.trim().split('\n').mapIndexed { index, value ->
             CheckableItem(index, value)
-        })
+        }
+        Log.i(TAG, "NewList:")
+        Log.i(TAG, newList.joinToString() { "Id: ${it.id} Message: ${it.message}" })
+        Log.i(TAG, "List")
+        Log.i(TAG, list.joinToString() { "Id: ${it.id} Message: ${it.message}" })
+
+
+        var maxId: Int = if (list.isEmpty()) 0 else list.maxOf { it.id }
+        /*
+            User deletes one (or more) items.
+            ------------------------------------
+            Here we need to first do list - newList to find
+            out the items deleted by the user. Then delete these items from list.
+        */
+
+        (list.toSet().minus(newList.toSet())).forEach { deletedItem ->
+            Log.i(TAG, "Removing item with Id: ${deletedItem.id} and message: ${deletedItem.message}")
+            list.removeAll { it == deletedItem }
+        }
+
+
+        /*
+            User adds one (or more) items.
+            ------------------------------------
+            First do newList - list to find out the items added by the user. Then add them to list.
+            Find the maximum id in the current list and then set its increments as ids
+            of the new items being added.
+         */
+
+        (newList.toSet().minus(list.toSet())).forEach { newItem ->
+            Log.i(TAG, "Adding new item with Id: ${newItem.id} and message>>${newItem.message}<<")
+            if (!list.isEmpty()) newItem.id = ++maxId
+            list.add(newItem)
+        }
+
+//        Log.i(TAG, list.joinToString() { "Id: ${it.id} Message: ${it.message}" })
+//        list.removeAll { true }
+//        list.addAll(value.text.split('\n').mapIndexed { index, value ->
+//            CheckableItem(index, value)
+//        })
+
     }
 
     fun onCheckedChange(checkableItem: CheckableItem, isChecked: Boolean) {
