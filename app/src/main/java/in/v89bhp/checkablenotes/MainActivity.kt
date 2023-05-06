e24778f7f8eccb9f8212e37ca20c6b1b73706e87
VaishakNair <vaishak.k.nair@gmail.com>
2023-05-06 21:06:44 +0530

    Continue theming logic.

--- app/src/main/java/in/v89bhp/checkablenotes/MainActivity.kt
+++ app/src/main/java/in/v89bhp/checkablenotes/MainActivity.kt
@@ -3,7 +3,6 @@ package `in`.v89bhp.checkablenotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import `in`.v89bhp.checkablenotes.ui.theme.CheckableNotesTheme
@@ -22,10 +21,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
     CheckableNotesTheme {
-        // A surface container using the 'background' color from the theme
-        Surface() {
-            content()
-        }
+        content()
     }
 }
 
