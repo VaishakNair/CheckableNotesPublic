2a57b8cc44ae807608258bd736449bcd4084f9fc
VaishakNair <vaishak.k.nair@gmail.com>
2023-07-23 18:23:08 +0530

    Continue...

--- app/src/test/java/in/v89bhp/checkablenotes/HomeViewModelUnitTest.kt
+++ app/src/test/java/in/v89bhp/checkablenotes/HomeViewModelUnitTest.kt
@@ -1,5 +1,10 @@
 import android.content.Context
+import androidx.compose.ui.text.input.TextFieldValue
+import `in`.v89bhp.checkablenotes.data.CheckableItem
+import `in`.v89bhp.checkablenotes.data.Note
+import `in`.v89bhp.checkablenotes.data.NotesRepository
 import `in`.v89bhp.checkablenotes.ui.home.HomeViewModel
+import kotlinx.coroutines.Dispatchers
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -9,11 +14,36 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class HomeViewModelUnitTest {
 
-    @Mock private val mockContext = mock<Context>()
+    @Mock
+    private val mockContext = mock<Context>()
 
 
     @Test
     fun loadNotesInitial() {
         val homeViewModel = HomeViewModel(FakeNotesRepository)
+    }
+}
+
+object FakeNotesRepository : NotesRepository(Dispatchers.IO) {
+    val fileNamesList = listOf("File1.json", "File2.json")
+    val notesList = listOf(
+        Note(
+            title = TextFieldValue("Note 1"), text = TextFieldValue("Item 1\nItem2"), list = listOf(
+                CheckableItem(id = 1, name = "Item 1", isChecked = false),
+                CheckableItem(id = 2, name = "Item 2", isChecked = true)
+            )
+        ),
+        Note(
+            title = TextFieldValue("Note 2"),
+            text = TextFieldValue("Item 3\nItem 4"),
+            list = listOf(
+                CheckableItem(id = 1, name = "Item 3", isChecked = false),
+                CheckableItem(id = 2, name = "Item 4", isChecked = false)
+            )
+        )
+    )
+
+    override suspend fun loadNotes(context: Context): Pair<List<String>, List<Note>> {
+        return fileNamesList to notesList
     }
 }
